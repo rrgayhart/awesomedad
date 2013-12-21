fd47d4f0d4edb873f187f0ed68d901413c6c92df
Meeka <rrgayhart@gmail.com>
2013-12-21 15:25:25 -0500

    Sets up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-facts = [ {name: "Musician", description: "He's an awesome musician", picture: "mom.jpg"},
-{name: "Handyman", description: "He can make anything", picture: "mom.jpg"},
-{name: "Electrical Genius", description: "He understands electricity!", picture: "mom.jpg"},
-{name: "Always Available", description: "He will always take time to talk about my problems", picture: "mom.jpg"},
-{name: "Great Artist", description: "He can draw just about anything", picture: "mom.jpg"},
+facts = [ {name: "Musician", description: "He's an awesome musician", picture: "music.jpg"},
+{name: "Handyman", description: "He can make anything", picture: "handy.jpg"},
+{name: "Fun Guy", description: "He knows how to have a good time", picture: "drinks.jpg"},
+{name: "Cool Uncle", description: "He's the cool uncle", picture: "kids.jpg"},
+{name: "Husband", description: "He's still married to my mom", picture: "withmom.jpg"},
+{name: "Fooseballer", description: "He is the foosball grand champion", picture: "fooseball.jpg"},
+{name: "Beer Enthusiast", description: "He can appreciate a good beer", picture: "goodbeer.jpg"},
+{name: "Bearded", description: "He's always rocked a righteous beard", picture: "beard.jpg"},
+{name: "Fix It Guy", description: "He can fix just about anything", picture: "fixer.jpg"},
+{name: "Always Available", description: "He will always take time to talk about my problems", picture: "dadstuff.jpg"},
+{name: "Great Artist", description: "He can draw just about anything", picture: "drawing.jpg"},
 {name: "Good Husband", description: "He let mom come to Colorado", picture: "mom.jpg"} ]
 
 facts.each do |fact|
