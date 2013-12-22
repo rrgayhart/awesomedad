7c758489efd1bb063e7c67d49cd5f1a2159819b4
Meeka <rrgayhart@gmail.com>
2013-12-22 15:19:57 -0500

    corrects routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Awesomedad::Application.routes.draw do
   resources :facts
-  get '/home' => 'facts#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'facts#index'
+  root 'facts#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
